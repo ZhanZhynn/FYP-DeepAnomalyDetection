e4a534b33689ffcc30de5c227806e44a163a0c86
Zz <zhanzhynn@gmail.com>
2023-03-14 19:49:19 +0800

    added size preview

--- components/Upload.vue
+++ components/Upload.vue
@@ -2,8 +2,8 @@
     <v-card>
         <v-card-actions class="justify-center" style="height: 24rem;">
             <div>
-                <v-file-input v-model="file" label="Upload File" prepend-icon="" multiple outlined color="primary"
-                    accept="text/xlsx"></v-file-input>
+                <v-file-input v-model="file" label="Upload File" prepend-icon="" multiple outlined show-size counter
+                    color="primary" accept="text/xlsx"></v-file-input>
                 <p>accepted file: .xlsx</p>
             </div>
         </v-card-actions>
