e115b478158d8bc2bc1e13caaf9c9738e5422dc9
Zz <zhanzhynn@gmail.com>
2023-03-14 19:18:56 +0800

    added upload component

---
+++ components/Upload.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card>
+        <v-card-actions class="justify-center" style="height: 24rem;">
+            <div>
+                <v-file-input v-model="file" label="Upload File" prepend-icon="" multiple outlined color="primary"
+                    accept="text/xlsx"></v-file-input>
+                <p>accepted file: .xlsx</p>
+            </div>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
