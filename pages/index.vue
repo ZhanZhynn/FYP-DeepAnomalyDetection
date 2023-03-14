ad445b868c99c8c3a949e8685d3f7c5b6060ed40
Zz <zhanzhynn@gmail.com>
2023-03-14 19:19:09 +0800

    updated main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,48 @@
 <template>
-  <div>
-    Hello World
+  <v-container>
+    <v-row>
+      <v-col cols="6">
+        <v-card style="height: 24rem; align-self: center;">
 
-  </div>
+          <v-card-actions class="justify-center">
+            <!-- <img src="../static/icon.png" :width="250" aspect-ratio="1/1" /> -->
+            <v-img src="https://blog.deemoney.com/content/images/2020/11/shutterstock_1456783511.jpg" height="200px"
+              cover></v-img>
+          </v-card-actions>
+          <v-card-title class="justify-center">
+            Financial Fraud Detection Software
+          </v-card-title>
+          <v-card-text class=text-center>
+            100% Open Source and Free
+          </v-card-text>
+        </v-card>
+
+      </v-col>
+      <v-col cols="6">
+        <Upload />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <Features />
+    </v-row>
+
+
+
+
+  </v-container>
 </template>
 
-<script setup>
+<script>
+import Upload from "../components/Upload.vue";
+import Features from "../components/Features.vue";
+
+export default {
+  components: {
+    Upload,
+    Features
+  }
+}
 
 </script>
 
