9df6b88e7475a8abd59d83ce13e2ddf715dfab65
Zz <zhanzhynn@gmail.com>
2023-03-14 19:18:34 +0800

    added features

---
+++ components/Features.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-container>
+        <v-row>
+            <h1>Our Features</h1>
+        </v-row>
+
+        <v-row>
+            <v-col cols="6">
+                <v-card-title class="text-h5">
+                    Open Source
+                    <v-icon>mdi-github</v-icon>
+                </v-card-title>
+
+
+                <v-card-subtitle>The web application is open sources. This means you can get the code file on github. No
+                    hanky-panky.</v-card-subtitle>
+            </v-col>
+
+            <v-col cols="6">
+                <v-card-title class="text-h5">
+                    High Accuracy
+                    <v-icon>mdi-bullseye-arrow</v-icon>
+                </v-card-title>
+
+                <v-card-subtitle>The model is trained on a custom-made Convolution Neural Network. Guaranteed to give you
+                    the best accuracy.</v-card-subtitle>
+            </v-col>
+        </v-row>
+
+        <v-row>
+            <v-col cols="6">
+                <v-card-title class="text-h5">
+                    Fast Processing
+                    <v-icon>mdi-clock-fast</v-icon>
+                </v-card-title>
+
+                <v-card-subtitle>We use enterprise grade servers to deliver the fastest and most stable
+                    performance.</v-card-subtitle>
+            </v-col>
+
+            <v-col cols="6">
+                <v-card-title class="text-h5">
+                    No Log Policy
+                    <v-icon>mdi-file-remove-outline</v-icon>
+                </v-card-title>
+
+                <v-card-subtitle>Your data is confidential. We have a no-log policy so your data is safe.</v-card-subtitle>
+            </v-col>
+        </v-row>
+
+
+    </v-container>
+</template>
+
+<script>
+
+</script>
+
+<style></style>
