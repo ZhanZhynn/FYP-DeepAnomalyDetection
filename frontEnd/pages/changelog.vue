8f1d1987954fc33c23f91be40f2e734084d7dac5
Zz <zhanzhynn@gmail.com>
2023-04-15 00:09:39 +0800

    updated changelog

--- frontEnd/pages/changelog.vue
+++ frontEnd/pages/changelog.vue
@@ -1,6 +1,10 @@
 <template>
     <v-row>
         <v-container>
+            <h2>Update 1.1</h2>
+            <!-- <v-card class="justify-center" style="height: 24rem; width: 48rem;"> -->
+            <p>-Updated support page to include email and FAQ.</p>
+
             <h2>Update 1.0</h2>
             <!-- <v-card class="justify-center" style="height: 24rem; width: 48rem;"> -->
             <p>-Added support page form submission.</p>
