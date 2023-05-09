b1570f947708b7b927fd3120cd79779a5e160892
Zz <zhanzhynn@gmail.com>
2023-05-09 23:13:26 +0800

    added no log policy requirement

--- frontEnd/pages/changelog.vue
+++ frontEnd/pages/changelog.vue
@@ -1,10 +1,12 @@
 <template>
     <v-row>
         <v-container>
-            <h2>Update 1.2</h2>
-            <p>-Integrated Machine Learning Model for Anomlay Detection.</p>
-            <p>-Fixed Analyse page not downloading output file correctly.</p>
+            <h2>Update 1.3</h2>
+            <p>-Implemented no log policy to remove user files from server immediately after processing.</p>
 
+            <h2>Update 1.2</h2>
+            <p>-Integrated Machine Learning Model for Anomaly Detection.</p>
+            <p>-Fixed Analyse page not downloading output file correctly.</p>
 
             <h2>Update 1.1</h2>
             <!-- <v-card class="justify-center" style="height: 24rem; width: 48rem;"> -->
