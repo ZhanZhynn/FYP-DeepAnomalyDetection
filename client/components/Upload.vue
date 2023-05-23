67166eee6ad777462f513bb1d51631c30ad4c008
Zz <zhanzhynn@gmail.com>
2023-05-23 19:31:44 +0800

    error catching

--- client/components/Upload.vue
+++ client/components/Upload.vue
@@ -176,6 +176,7 @@ export default {
 
                 })
                 .catch((error) => {
+                    console.log(error)
                     console.error(error);
                 });
         },
